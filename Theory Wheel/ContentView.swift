b89d7bdd1975d5505431a6305d29c895a674ae9f
mightknow <michaeljohnbackes@gmail.com>
2020-09-29 17:10:22 -0500

    Initial Commit

---
+++ Theory Wheel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Theory Wheel
+//
+//  Created by mightknow on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
