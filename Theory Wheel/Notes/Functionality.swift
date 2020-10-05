58709bfdfec4fce8912c8c813c3a181fcd8f7a90
mightknow <michaeljohnbackes@gmail.com>
2020-10-05 13:34:35 -0500

    Add to Notes

---
+++ Theory Wheel/Notes/Functionality.swift
@@ -0,0 +1,61 @@
+//
+//  Functionality.swift
+//  Theory Wheel
+//
+//  Created by mightknow on 9/29/20.
+//
+
+import Foundation
+
+/*
+ 
+ GENERAL:
+ 
+ Wheel to select key
+ Display Info
+ Info/Audio mode toggle
+ (Audio mode) Play note/chord on tap?
+ (Info mode) every element in the display is tappable to display more info.
+ 
+ */
+
+/*
+ 
+ INFO TO DISPLAY:
+ 
+ Key Center Major
+ Key Center Minor
+ Adjacent Key centers for both
+ All 7 Notes of scale
+ Chord quality based on 7 notes, with notes that make chord
+ Symbol for chord of each scale degree (Major and minor)
+ Key signature (number of sharps or flats)
+ Lines connecting key center to keys that highlights I, IV, V (and subhighlights ii in Major?, bVI in minor?)
+ Include labeling for Harmonic Minor V chord?
+ 
+ */
+
+/*
+ 
+ NAVIGATION:
+ 
+ Launch Screen
+ Onboarding (Launch Screen with forced acknowledgement)
+ Main
+ Options
+ 
+ */
+
+/*
+ 
+ OPTIONS:
+ 
+ Toggle which is on top (and starting order) between Major/minor
+ How far to extend chords (triads, 7th, 9th)
+ Key select wheel moves in 1/2 steps or circle of 4ths
+ Symbol type (classical, Nashville Numbers, etc)
+ Change color scheme?
+ 
+ */
+
+
