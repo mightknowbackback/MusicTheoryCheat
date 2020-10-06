dece5cc2d1c73fe6b11b09350a22ef47a4c4c9fc
mightknow <michaeljohnbackes@gmail.com>
2020-10-06 15:19:22 -0500

    Add test for PitchClass instance method stringLiteral

--- Theory WheelTests/Theory_WheelTests.swift
+++ Theory WheelTests/Theory_WheelTests.swift
@@ -9,6 +9,31 @@ import XCTest
 @testable import Theory_Wheel
 
 class Theory_WheelTests: XCTestCase {
+    
+    func testPitchClassStringLiterals() {
+        
+        let eSharpString = "E" + sharpSymbol
+        let f : PitchClass = .f
+        XCTAssertEqual(f.stringLiteral(inKey: .gFlat, withKeySpelling: .sharps), eSharpString)
+        
+        let fString = "F"
+        XCTAssertEqual(f.stringLiteral(inKey: .gFlat, withKeySpelling: .flats), fString)
+        
+        let cFlatString = "C" + flatSymbol
+        let b : PitchClass = .b
+        XCTAssertEqual(b.stringLiteral(inKey: .gFlat), cFlatString)
+        
+        let bString = "B"
+        XCTAssertEqual(b.stringLiteral(inKey: .gFlat, withKeySpelling: .sharps), bString)
+        
+        let fSharpString = "F" + sharpSymbol
+        let fSharp : PitchClass = .gFlat
+        XCTAssertEqual(fSharp.stringLiteral(withKeySpelling: .sharps), fSharpString)
+        
+        let gFlatString = "G" + flatSymbol
+        XCTAssertEqual(fSharp.stringLiteral(withKeySpelling: .flats), gFlatString)
+        
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
