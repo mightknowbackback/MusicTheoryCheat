7035d26d67bd07e46059b2e7eaa8fcf41bb1a90c
mightknow <michaeljohnbackes@gmail.com>
2020-10-07 02:51:54 -0500

    Begin moving renamed files to new git

--- Constants.swift
+++ Constants.swift
@@ -14,3 +14,4 @@ let diminishedSymbol : String = "º"
 let majorScaleIntervals : [Int] = [0, 2, 4, 5, 7, 9, 11]
 let minorScaleIntervals : [Int] = [0, 2, 3, 5, 7, 8, 10]
 let majorRomanNumerals : [String] = ["I", "ii", "iii", "IV", "V", "vi", "vii" + diminishedSymbol]
+// Changes...
