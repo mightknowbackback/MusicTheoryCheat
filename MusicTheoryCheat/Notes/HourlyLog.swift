8b5adec4d9e6e12824aee0e9a66af09b47d9c060
mightknow <michaeljohnbackes@gmail.com>
2020-10-13 22:38:27 -0500

    Add HourlyLog notes

---
+++ MusicTheoryCheat/Notes/HourlyLog.swift
@@ -0,0 +1,22 @@
+//
+//  HourlyLog.swift
+//  MusicTheoryCheat
+//
+//  Created by mightknow on 10/13/20.
+//
+
+import Foundation
+
+/*
+ 
+ CODING:
+ 10/06/20 - 8 hrs
+ 10/08/20 - 4 hrs
+ 10/09/20 - 4 hrs
+ 10/12/20 - 3 hrs
+ 10/13/20 - 5 hrs
+ 
+ DESIGN:
+ 10/05/20 - 8 hrs
+ 
+ */
